4831378ce59054026fb388a13f13493d5af858ab
Memopus <yassinesaq2007@gmail.com>
2023-08-07 20:26:39 +0100

    Add new section

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ icon.addEventListener("click", () => {
   if (responsiveDiv.classList.contains("active")) {
     responsiveDiv.style.display = "none";
     responsiveDiv.classList.remove("active");
+    icon.classList.remove("fa-x");
+    icon.classList.add("fa-bars");
   } else if (!responsiveDiv.classList.contains("active")) {
     responsiveDiv.style.display = "block";
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-x");
     responsiveDiv.classList.add("active");
   }
 });
